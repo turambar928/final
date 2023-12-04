0dc6c21b6f31d22be7bad759d92b8e5e17d4c667
10086hu <15727198260@163.com>
2023-12-04 23:57:49 +0800

    基础更新

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -35,10 +35,11 @@ using namespace cocos2d::experimental;
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(480,320);            
-static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);     
+static cocos2d::Size designResolutionSize = cocos2d::Size(1024, 768);      //调整窗口的大小，游戏的基础分辨率
+static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);          //针对具有较小屏幕的设备（如手机）的分辨率。
+static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);        //针对具有中等屏幕的设备（如平板电脑）的分辨率。
+static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);        //针对具有大屏幕的设备（如电视或高分辨率的显示器）的分辨率。
+      //后三个均未进行修改
 
 AppDelegate::AppDelegate()
 {
@@ -68,13 +69,13 @@ static int register_all_packages()
     return 0; //flag for packages manager
 }
 
-bool AppDelegate::applicationDidFinishLaunching() {
+bool AppDelegate::applicationDidFinishLaunching() {          //游戏启动运行时所使用的接口，主要修改部分
     // initialize director
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glview = GLViewImpl::createWithRect("project2", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+        glview = GLViewImpl::createWithRect("Carrot Fantasy", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));   //初始窗口的名称
 #else
         glview = GLViewImpl::create("project2");
 #endif
@@ -85,7 +86,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     director->setDisplayStats(true);
 
     // set FPS. the default value is 1.0/60 if you don't call this
-    director->setAnimationInterval(1.0f / 60);           //刷新率
+    director->setAnimationInterval(1.0f / 60);           //设置了刷新率的上限，此时为最高60帧
 
     // Set the design resolution
     glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
@@ -109,28 +110,32 @@ bool AppDelegate::applicationDidFinishLaunching() {
     register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+    auto scene = HelloWorld::createScene();               //创建初始的场景，初始化
 
     // run
-    director->runWithScene(scene);
+    director->runWithScene(scene);      //导演从此场景启动
 
     return true;
 }
 
 // This function will be called when the app is inactive. Note, when receiving a phone call it is invoked.
-void AppDelegate::applicationDidEnterBackground() {
-    Director::getInstance()->stopAnimation();
-
-#if USE_AUDIO_ENGINE
+void AppDelegate::applicationDidEnterBackground() {    //将游戏切入后台
+    Director::getInstance()->stopAnimation();        //游戏被暂停
+                                     
+#if USE_AUDIO_ENGINE                         //有音乐的话，音乐也暂停
     AudioEngine::pauseAll();
 #endif
+
+    //AudioEngine::pauseAll();   可以直接改为此
 }
 
 // this function will be called when the app is active again
-void AppDelegate::applicationWillEnterForeground() {
-    Director::getInstance()->startAnimation();
+void AppDelegate::applicationWillEnterForeground() {         //又被切换回前端
+    Director::getInstance()->startAnimation();           //动作重新开始运行
 
-#if USE_AUDIO_ENGINE
+#if USE_AUDIO_ENGINE                            //音乐重新开始运行
     AudioEngine::resumeAll();
 #endif
+
+    //AudioEngine::resumeAll();    可直接改为此
 }
